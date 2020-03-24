eb14717ed8f1563c99bf897cbb4ac05847ef0d3f
Yasir-Mohamud <ymohamed018@gmail.com>
2020-03-24 13:27:42 -0700

    doing feature 5

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # book_app
+
+
+Number and name of feature: 1
+
+Estimate of time needed to complete: 1hr
+
+Start time: 10:30
+
+Finish time: 11:05
+
+Actual time needed to complete: 35mins
+
+Number and name of feature: 2
+
+Estimate of time needed to complete: 30min
+
+Start time: 11:05
+
+Finish time: 11:20
+
+Actual time needed to complete: 15mins
+
+Number and name of feature: 3
+
+Estimate of time needed to complete: 30min
+
+Start time: 11:20
+
+Finish time: 11:20
+
+Actual time needed to complete: 15mins
+
--- server.js
+++ server.js
@@ -1,21 +1,60 @@
 'use strict';
 const express = require('express');
 const app = express();
+const superagent = require('superagent');
 require('dotenv').config();
 const ejs = require('ejs');
 const PORT = process.env.PORT || 3001;
-console.log('app',app)
 
 /////////// MIDDLEWARE ////////
 // tells express that we are using ejs as our templating system
 app.set('view engine', 'ejs');
+// parses our request.body so that we can read form data when it comes in
+app.use(express.urlencoded({extended:true}));
 // serve my static files from my public directory
 app.use(express.static('./public'));
+
+app.get('/bananas' , (request,response) => {
+  response.render('pages/searches/new.ejs')
+})
+
+app.post('/searches' , (request,response) => {
+  console.log('hello',request.body);
+  let searchItem = request.body.search[0];
+  let titleorauthor = request.body.search[1];
+  let url  = 'https://www.googleapis.com/books/v1/volumes?q=';
+  if (titleorauthor === 'title') {
+    url+=`+intitle:${searchItem}`
+  } else if (titleorauthor === 'author') {
+    url+=`+inauthor:${searchItem}`
+  }
+  superagent(url)
+    .then( superagentResults => {
+      console.log(superagentResults.body.items[0].volumeInfo)
+      let bookArr = superagentResults.body.items;
+      let finalBookArr = bookArr.map(book => {
+        return new Book (book.volumeInfo, book.imageLinks);
+      })
+      response.send(finalBookArr.slice(0,10));
+    })
+
+})
+
+////// google api book ///////
+
+function Book (obj) {
+  this.img = 
+  this.authors = obj.authors;
+  this.title = obj.title;
+  this.description = obj.description;
+
+}
+
 
 app.get('/' , (request,response) => {
   response.render('pages/index.ejs')
 })
-console.log(PORT)
+
 app.listen( PORT, () => {
   console.log(`listening on port ${PORT}`);
-})
+});
